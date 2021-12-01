04e2a8645d0b23ab7c47cd2cdd013595bae6873b
05615 <74238913+05615@users.noreply.github.com>
2021-12-01 10:30:45 +0000

    Update workingforecast.js

--- workingforecast.js
+++ workingforecast.js
@@ -23,44 +23,56 @@ function displayWeather(input) {
     let url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&appid=dbe735587bad3551fa97c03750d20f1c";
     fetch(url, {}).then(response => {
         return response.json();                 //now return that promise to JSON
-}).then(response => {
+    }).then(response => {
         console.log(response);
-    let name = (response["name"]);
-    let coordlat = (response["coord"]["lat"]);
-    let coordlon = (response["coord"]["lon"]);
-    let temperature = (response["main"]["feels_like"]+ "°C");
-    let actualtemp = (response["main"]["temp"]+ "°C");
-    let humidity = (response["main"]["humidity"]+ "%");
-    let tempmin = (response["main"]["temp_min"]+ "°C");
-    let tempmax = (response["main"]["temp_max"]+ "°C");
-    let weather = (response["weather"]["0"]["main"]);
-    let description = (response["weather"]["0"]["description"]);
-    let icon = (response["weather"]["0"]["icon"]);
-    let wind = (response["wind"]["speed"]);
-    let sunrise = (response["sys"]["sunrise"]);
-    let sunset = (response["sys"]["sunset"]);
-    let unixTimestamp = sunrise;
-    let milliseconds = unixTimestamp * 1000
-    let dateObject = new Date(milliseconds)
-    let humanDateFormat = dateObject.toLocaleString();//10:30:15
-    console.log(dateObject.toLocaleString("en-US", {hour: "numeric"})); // 10 AM
-    console.log(dateObject.toLocaleString("en-US", {minute: "numeric"})); // 30
-    console.log(dateObject.toLocaleString("en-US", {second: "numeric"})); // 15
-    let unixTimestamp2 = sunset;
-    let milliseconds2 = unixTimestamp2 * 1000;
-    let dateObject2 = new Date(milliseconds2);
-    let humanDateFormat2 = dateObject2.toLocaleString();
-    console.log(dateObject2.toLocaleString("en-US", {hour: "numeric"})); // 10 AM
-    console.log(dateObject2.toLocaleString("en-US", {minute: "numeric"})); // 30
-    console.log(dateObject2.toLocaleString("en-US", {second: "numeric"})); // 15
-    let w1 = new storeweather(name,coordlat,coordlon,temperature,actualtemp,humidity,tempmin,tempmax,weather,description,icon,wind,sunrise,sunset,humanDateFormat,humanDateFormat2)
-    console.log(w1);
-    let div = document.getElementById("ApiData");
-    div.innerHTML = " ";
-    div.innerHTML += [w1.name, w1.coordlat, w1.coordlon, w1.actualtemp, w1.description, w1.humanDateFormat, w1.humanDateFormat2, w1.humidity, w1.icon, w1.sunrise, w1.sunset, w1.temperature, w1.tempmax, w1.tempmin, w1.weather, w1.wind, w1.icon,];
+        let name = (response["name"]);
+        let coordlat = (response["coord"]["lat"]);
+        let coordlon = (response["coord"]["lon"]);
+        let temperature = (response["main"]["feels_like"]+ "°C");
+        let actualtemp = (response["main"]["temp"]+ "°C");
+        let humidity = (response["main"]["humidity"]+ "%");
+        let tempmin = (response["main"]["temp_min"]+ "°C");
+        let tempmax = (response["main"]["temp_max"]+ "°C");
+        let weather = (response["weather"]["0"]["main"]);
+        let description = (response["weather"]["0"]["description"]);
+        let icon = (response["weather"]["0"]["icon"]);
+        let wind = (response["wind"]["speed"]);
+        let sunrise = (response["sys"]["sunrise"]);
+        let sunset = (response["sys"]["sunset"]);
+        let unixTimestamp = sunrise;
+        let milliseconds = unixTimestamp * 1000
+        let dateObject = new Date(milliseconds)
+        let humanDateFormat = dateObject.toLocaleString();//10:30:15
+        console.log(dateObject.toLocaleString("en-US", {hour: "numeric"})); // 10 AM
+        console.log(dateObject.toLocaleString("en-US", {minute: "numeric"})); // 30
+        console.log(dateObject.toLocaleString("en-US", {second: "numeric"})); // 15
+        let unixTimestamp2 = sunset;
+        let milliseconds2 = unixTimestamp2 * 1000;
+        let dateObject2 = new Date(milliseconds2);
+        let humanDateFormat2 = dateObject2.toLocaleString();
+        console.log(dateObject2.toLocaleString("en-US", {hour: "numeric"})); // 10 AM
+        console.log(dateObject2.toLocaleString("en-US", {minute: "numeric"})); // 30
+        console.log(dateObject2.toLocaleString("en-US", {second: "numeric"})); // 15
+        let w1 = new storeweather(name,coordlat,coordlon,temperature,actualtemp,humidity,tempmin,tempmax,weather,description,icon,wind,sunrise,sunset,humanDateFormat,humanDateFormat2)
+        console.log(w1);
+        let div = document.getElementById("ApiData");
+       // div.innerHTML = " ";
+       // div.innerHTML += [w1.name, w1.coordlat, w1.coordlon, w1.actualtemp, w1.description, w1.humanDateFormat, w1.humanDateFormat2, w1.humidity, w1.icon, w1.sunrise, w1.sunset, w1.temperature, w1.tempmax, w1.tempmin, w1.weather, w1.wind, w1.icon,];
+
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&appid=dbe735587bad3551fa97c03750d20f1c", function (data){
+            console.log(data)
+
+            var icon =  "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            var temp = Math.floor(data.main.temp) + "°C";
+            var weather = data.weather[0].main;
 
 
-});
+            $('.icon').attr('src',icon);
+            $('.weather').append(weather);
+            $('.temp').append(temp);
+        });
+
+    });
 
 }
 
@@ -70,12 +82,5 @@ function search() {
     displayWeather(location);
 }
 
-
-function setup() {
-    createCanvas(400, 400);
-    background(220);
-}
-
-function draw() {
-    text() ;
-}
+const panoImage = document.querySelector('.pano-image');
+const panoCity = 'https://unsplash.com/images/stock/hdr';
